7a9f70efcfbe7f9d12c1040fd91a638db8b656cf
gabriel geoffrey henderso <gabe1980@LinuxLab07.byui.edu>
2017-05-10 18:27:39 -0600

    initial rational

--- rational.h
+++ rational.h
@@ -1,11 +1,26 @@
 /***************************************************************
  * File: rational.h
- * Author: (your name here)
+ * Author: Gabriel Partridge
  * Purpose: Contains the definition of the Rational class
  ***************************************************************/
 #ifndef RATIONAL_H
 #define RATIONAL_H
 
-// put your class definition here
+/************************************
+ * Rational Class
+ * Stores a rational number (fraction) 
+ *************************************/
+class Rational
+{
+  private:
+   int top;
+   int bottom;
+
+  public:
+   void prompt();
+   Void display();
+   void displayDecimal();
+   
+};
 
 #endif
